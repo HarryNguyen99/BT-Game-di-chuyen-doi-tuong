42c888834c8f57990a2b8e18be31f7081ae4c247
Harry Nguyen <50328363+HarryNguyen99@users.noreply.github.com>
2020-03-04 13:52:54 +0700

    v1

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-/**
- * Created by nhatnk on 4/26/17.
- */
+function Hero(image, top, left, size) {
+    this.image = image;
+    this.top = top;
+    this.left = left;
+    this.size = size;
 
-function Hero(image, top, left, size){
-  this.image = image;
-  this.top = top;
-  this.left = left;
-  this.size = size;
+    this.getHeroElement = function () {
+        return '<img width="' + this.size + '"' +
+            ' height="' + this.size + '"' +
+            ' src="' + this.image + '"' +
+            ' style="top: ' + this.top + 'px; left:' + this.left + 'px; position:absolute;" />';
+    }
 
-  this.getHeroElement = function(){
-    return '<img width="'+ this.size + '"' +
-      ' height="'+ this.size + '"' +
-      ' src="' + this.image +'"' +
-      ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
-  }
-
-  this.moveRight = function(){
-    this.left += 20;
-    console.log('ok: ' + this.left);
-  }
+    this.moveRight = function () {
+        this.left += 50;
+        console.log('ok: ' + this.left);
+    }
 
 }
 
-var hero = new Hero('pikachu.png', 20, 30, 200);
+let hero = new Hero('songoku.png', 20, 0, 200);
 
-function start(){
-  if(hero.left < window.innerWidth - hero.size){
-    hero.moveRight();
-  }
-  document.getElementById('game').innerHTML = hero.getHeroElement();
-  setTimeout(start, 500)
+function start() {
+    if (hero.left < window.innerWidth - hero.size) {
+        hero.moveRight();
+    }
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    setTimeout(start, 500)
 }
+
 
 start();
